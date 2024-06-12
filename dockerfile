54e997ca884f15274da1499fc8d656961c49278e
rgb234 <fehs0611@gmail.com>
2024-06-12 16:57:45 +0900

    cleanup useless directory, comments

--- dockerfile
+++ dockerfile
@@ -4,16 +4,19 @@
 FROM node:15.12.0-alpine3.12
 #WORKDIR in container, app directory construction
 WORKDIR /usr/src/app
-#install dependencies, package-.json, pacakage-lock.json
-# That is recommended to copy package*.json before "npm install"
-# https://bitjudo.com/blog/2014/03/13/building-efficient-dockerfiles-node-dot-js/
-# It prevents to copy same modules everytime when the change occurs to package list
-# cashed docker layer skip that duplicated parts
+
+# install dependencies
+# That is recommended to copy package*.json before application installation
+# ref. https://bitjudo.com/blog/2014/03/13/building-efficient-dockerfiles-node-dot-js/
+# "When possible, docker will try to use an existing cached layer if it’s possible. 
+# You should try to take advantage of layers as much as possible by organizing your commands in a specific order. ""
 COPY package*.json ./
-#install dependencies
 RUN npm install
-#copy app files from local directory where Dockerfile is to container WORKDIR
+
+# install application files
+# copy app files from local directory where Dockerfile is to container WORKDIR
 COPY . .
+
 #docker daemon port mapping
 EXPOSE 3000
 #runtime, WORKDIR/js/app.js execute
--- main.js
+++ main.js
@@ -1,43 +1,43 @@
-const express = require('express');
-const request = require('request');
-const cors = require('cors');
+const express = require("express");
+const request = require("request");
+const cors = require("cors");
 
 const API_CLIENT_ID = "tMfKtRmuy_fJa5dxsAFa";
 const API_CLIENT_SECRET = "3KTsM8LPGr";
 
 const app = express();
 
-// Middleware for POST request 
+// Middleware for POST request
 //json데이터를 담은 request body 를 parsing 하여 받아들일 수 있게 함
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req, res) => {
-    res.send(`<h1>Hello World</h1>`);
+app.get("/", (req, res) => {
+  res.send(`<h1>Hello World</h1>`);
 });
 
 //reference(API-request) : https://velog.io/@mingtorr/node.js-%EC%97%90%EC%84%9C-api%EC%9A%94%EC%B2%AD%ED%95%98%EA%B8%B0
 
-app.get('/:keyword', (req, res) => {
-    const options = {
-        uri: "https://openapi.naver.com/v1/search/book.json",
-        qs: {
-            query: req.params.keyword, //:keyword
-            display: 100,
-        },
-        headers: {
-            "X-Naver-Client-Id": API_CLIENT_ID,
-            "X-Naver-Client-Secret": API_CLIENT_SECRET,
-        },
-    };
-    request(options, function (err, response, body) {
-        books_body = JSON.parse(response.body);
-        //response.body["items"] >> error :"undefined" is not valid JSON
-        books = books_body["items"];
-        res.send(books);
-    });
+app.get("/:keyword", (req, res) => {
+  const options = {
+    uri: "https://openapi.naver.com/v1/search/book.json",
+    qs: {
+      query: req.params.keyword, //:keyword
+      display: 100,
+    },
+    headers: {
+      "X-Naver-Client-Id": API_CLIENT_ID,
+      "X-Naver-Client-Secret": API_CLIENT_SECRET,
+    },
+  };
+  request(options, function (err, response, body) {
+    books_body = JSON.parse(response.body);
+    //response.body["items"] >> error :"undefined" is not valid JSON
+    books = books_body["items"];
+    res.send(books);
+  });
 });
 
 app.listen(3000, () => {
-    console.log("Listening on port:3000")
+  console.log("Listening on port:3000");
 });
